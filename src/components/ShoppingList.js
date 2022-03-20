2af6c66863740863c449cd46f3fa9b642e560ef8
Jarod Wilson <jaroddwilson@gmail.com>
2022-03-20 10:45:38 -0400

    Implements search by word functionality

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,12 +5,19 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchWord, setSearchWord] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleSearchChange(event) {
+    setSearchWord(event.target.value)
+  }
+
+  const wordFilteredItems = items.filter((item) => item.name.includes(searchWord))
+
+  const itemsToDisplay = wordFilteredItems.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
@@ -19,7 +26,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
